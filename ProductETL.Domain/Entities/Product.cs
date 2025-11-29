f2c8b3ac44c1f34bc30c3ead878897e964d5b93f
Maycon Jardim <mayconsmill@gmail.com>
2025-11-29 16:40:38 -0300

    chore: criando construtores para as entidades

--- ProductETL.Domain/Entities/Product.cs
+++ ProductETL.Domain/Entities/Product.cs
@@ -3,10 +3,22 @@
     public sealed class Product
     {
         public Guid Id { get; private set; }
-        public required string PN { get; set; } 
-        public string Manufacturer { get; set; } = string.Empty;
+        public string PN { get; private set; } 
+        public string Manufacturer { get; private set; } = string.Empty;
         public string Category { get; private set; } = string.Empty;
         public DateTime CreatedAt { get; private set; }
         public ICollection<SupplierListing> SupplierListings { get; private set; } = new List<SupplierListing>();
+
+        public Product(string pn, string manufacture, string category)
+        {
+            if (string.IsNullOrWhiteSpace(pn))
+                throw new Exceptions.BusinessRuleException("Número de série é requerido!");
+
+            Id = Guid.CreateVersion7();
+            PN = pn;
+            Manufacturer = manufacture;
+            Category = category;
+            CreatedAt = DateTime.UtcNow;
+        }
     }
 }
--- ProductETL.Domain/Entities/SupplierListing.cs
+++ ProductETL.Domain/Entities/SupplierListing.cs
@@ -1,15 +1,44 @@
-﻿namespace ProductETL.Domain.Entities
+﻿using ProductETL.Domain.Exceptions;
+
+namespace ProductETL.Domain.Entities
 {
     public class SupplierListing
     {
-        public Guid Id { get; set; }
-        public required string PN { get; set; }
-        public decimal Price { get; set; }
-        public string Currency { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public int StockQuantity { get; set; }
-        Dictionary<string, string> Specs { get; set; } = new();
-        public DateTime CreatedAt { get; set; }
-        public DateTime ValidUntil { get; set; }
+        public Guid Id { get; private set; }
+        public string PN { get; private set; }  
+        public decimal Price { get; private set; }
+        public string Currency { get; private set; } = string.Empty;
+        public string Description { get; private set; } 
+        public int StockQuantity { get; private set; }
+        public IReadOnlyDictionary<string, string> Specs => _specs;
+        private readonly Dictionary<string, string> _specs = new();
+        public DateTime CreatedAt { get; private set; }
+        public DateTime ValidUntil { get; private set; }
+
+        public SupplierListing(string pn, decimal price, string curreny, string description, int quantity, Dictionary<string, string> specs, DateTime validUntil)
+        {
+            if (string.IsNullOrWhiteSpace(pn))
+                throw new BusinessRuleException("O Número de série é requerido!");
+
+            if (price <= 0)
+                throw new BusinessRuleException("O Preço deve ser maior que zero!");
+
+
+            if (string.IsNullOrWhiteSpace(curreny))
+                throw new Exceptions.BusinessRuleException("A Moeda é requerida!");
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new Exceptions.BusinessRuleException("A descrição é requerida!");
+
+            Id = Guid.CreateVersion7();
+            PN = pn;
+            Price = price;
+            Currency = curreny;
+            Description = description;
+            StockQuantity = quantity;
+            _specs = specs ?? new Dictionary<string, string>();
+            ValidUntil = validUntil;
+            CreatedAt = DateTime.UtcNow;
+        }
     }
 }
