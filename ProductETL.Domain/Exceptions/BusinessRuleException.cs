c29f42562f4fdac2c110a202156170dc41804cbc
Maycon Jardim <mayconsmill@gmail.com>
2025-11-29 16:19:30 -0300

    chore: criando classe personalizada de Exceptions

---
+++ ProductETL.Domain/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductETL.Domain.Exceptions
+{
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException(string message) : base (message){}
+    }
+}
